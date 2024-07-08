2b90d285ab40f2e4fbd688bbb4c8cbe1a8faf1db
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-07-08 22:53:29 +0600

    Unnecessary file delete

--- src/app/(WithLayout)/profile/[id]/loading.js
+++
@@ -1,6 +0,0 @@
-import Loader from "@/components/shared/loading/Loader";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <Loader />;
-}
--- src/app/(WithLayout)/profile/page.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-const page = () => {
-  return redirect("/");
-};
-
-export default page;

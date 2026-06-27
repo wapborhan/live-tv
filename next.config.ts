eb80f51893b30034a4935dbeddc1b4ce8086d352
Borhan Uddin <borhanuddin979@gmail.com>
2026-06-27 18:48:47 +0600

    Deployment relatated issue fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/live-tv" : "",
   /* config options here */
 };
 
